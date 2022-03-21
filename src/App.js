4fae25a7c8c5cc2d96f6d6431fbbc984970a43b4
sp5255 <pundir5255@gmail.com>
2022-03-21 10:32:12 +0530

    changes in App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import Home from "./components/Home";
+
+
+import TopNav from "./TopNav";
 import MoviesContainer from "./components/MoviesContainer";
-
-
 import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">            
-            <Routes>
-                <Route path="/" element={<Home />} />
+        <TopNav />
+            <Routes>                
                 <Route path="/search/:query" element={<MoviesContainer />} />
             </Routes>
         </div>
